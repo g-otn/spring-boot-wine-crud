eb90c83e512559375c1140cc330b0b4e6f2a5d67
g-otn <g-otn@hotmail.com>
2019-09-18 10:40:39 -0300

    Adicionar logout

--- src/main/java/com/algaworks/vinho/config/SecurityConfig.java
+++ src/main/java/com/algaworks/vinho/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -27,8 +28,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                     .anyRequest().authenticated()
-                .and()
-                .formLogin();
+                    .and()
+                .formLogin()
+                    .and()
+                .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
     }
 
 }
