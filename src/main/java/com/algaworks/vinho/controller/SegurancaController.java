eddf4009f4959fd3d9fe774f70545d8b1e2dc9d8
g-otn <g-otn@hotmail.com>
2019-09-18 11:17:56 -0300

    Adicionar redirecionamento para autenticados e mensagem de erro de autenticacao

--- src/main/java/com/algaworks/vinho/controller/SegurancaController.java
+++ src/main/java/com/algaworks/vinho/controller/SegurancaController.java
@@ -1,5 +1,7 @@
 package com.algaworks.vinho.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SegurancaController {
 
     @RequestMapping("/login")
-    public String login() {
-        return "Login.html";
+    public String login(@AuthenticationPrincipal User user) {
+        if (user != null) { // Ja logado
+            return "redirect:/vinhos";
+        }
+
+        return "Login";
     }
 
 }
